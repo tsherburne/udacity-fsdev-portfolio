0b105991c59d5174018ceee6c55389f52f5d3071
Tim Sherburne <sherburne.tim@gmail.com>
2018-01-06 21:43:23 +0000

    Initial version of Portfolio project.

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+app.use(express.static('public'));
+
+app.listen(PORT, () => console.log('Example app listening on port:' + PORT));
